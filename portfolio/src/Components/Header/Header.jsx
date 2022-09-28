26696e3861238a3f25b4ecabc9153e2a3a53cf2e
Johnny Faria <johnnys.faria@gmail.com>
2022-09-28 17:51:54 -0300

    Finalização home

--- portfolio/src/Components/Header/Header.jsx
+++ portfolio/src/Components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import styles from './Header.module.css';
 import Container from '../Container/Container';
+import face from '../Img/icon face.png';
+import insta from '../Img/icon Insta.png';
+import twitter from '../Img/icon twitter.png';
 
 export default function Header() {
   return (
     <Container>
       <div className={styles.container}>
-        <h1>LOGO</h1>
+        <h1>{''}</h1>
         <ul className={styles.list}>
           <li>
             <a href="#about">About me</a>
@@ -17,6 +20,17 @@ export default function Header() {
             <a href="#contact">Contact</a>
           </li>
         </ul>
+        <div className={styles.social}>
+          <a href="https://www.facebook.com/nicolasferreirac/">
+            <img src={face} alt="" />
+          </a>
+          <a href="https://www.instagram.com/nicolas.ferreirac/">
+            <img src={insta} alt="" />
+          </a>
+          <a href="https://twitter.com/NicolasFereirac">
+            <img src={twitter} alt="" />
+          </a>
+        </div>
       </div>
     </Container>
   );
--- portfolio/src/Pages/Home/Home.jsx
+++ portfolio/src/Pages/Home/Home.jsx
@@ -6,11 +6,25 @@ export default function Home() {
   return (
     <>
       <section>
+        <div className={styles.title}>
+          <h1>DESIGN</h1>
+          <span>Gráfico</span>
+          <img className={styles.principal} src={backnick} alt="background" />
+        </div>
         <div className={styles.card}>
           <div>
-            <img className={styles.photo} src={Nick} alt="Foto do artista" />
-            <img className={styles.back} src={backnick} alt="imagem de fundo" />
+            <img className={styles.photo} src={Nick} alt="photoDesigner" />
+            <img className={styles.back} src={backnick} alt="background" />
           </div>
+          <p>Nicolas Ferreira</p>
+          <blockquote>
+            Facilito a comunicação com seu cliente através do design!
+          </blockquote>
+          <button>
+            <a href="http://api.whatsapp.com/send?1=pt_BR&phone=5511920151566">
+              Vamos conversar
+            </a>
+          </button>
         </div>
       </section>
     </>
